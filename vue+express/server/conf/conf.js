3b8a9c45236984cbb901cb9346e32f513528896a
kd2888 <you@example.com>
2018-06-05 17:48:07 +0800

    no message

--- vue+express/server/conf/conf.js
+++ vue+express/server/conf/conf.js
@@ -2,7 +2,7 @@ module.exports = {
     mysql: {
         host: '116.196.102.192',
         user: 'root',
-        password: '981407034',
+        password: '',
         database:'user',
         // 最大连接数，默认为10
         connectionLimit: 10
